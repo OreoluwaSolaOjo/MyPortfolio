27174f92f1206d64e634d93301dcccb0f54a3a64
Oreoluwa <solaojoore@yahoo.com>
2021-11-11 18:38:55 +0100

    Changes made

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = False
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(",")
