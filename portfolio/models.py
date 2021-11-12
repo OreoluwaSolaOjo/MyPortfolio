cb37a05631f408bedce678ea62c5aaa142790f66
Oreoluwa <solaojoore@yahoo.com>
2021-11-12 07:42:40 +0100

    Changes made

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,9 +5,9 @@ from django.db import models
 
 
 class Contact(models.Model):
-    txtName = models.TextField()
-    txtEmail = models.TextField()
-    txtPhone = models.TextField()
+    txtName = models.CharField(max_length=200)
+    txtEmail = models.EmailField(max_length=200, blank=True)
+    txtPhone = models.CharField(max_length=200)
     txtMsg = models.TextField()
 
     def __str__(self):
